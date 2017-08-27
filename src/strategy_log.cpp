df4213ccbcb877ae05593776463a0394dfb98b85
wangying <17199883@qq.com>
2017-08-27 20:41:59 +0800

    update codes for x-trader

--- src/strategy_log.cpp
+++ src/strategy_log.cpp
@@ -1,5 +1,8 @@
 #include "strategy_log.h"
 
+struct Log1 StrategyLog::log1_[8388608];
+struct Log2 StrategyLog::log2_[8388608];
+
 void StrategyLog::Log1(int strategy_id, struct Log1 &content)
 {
 	// TODO:
