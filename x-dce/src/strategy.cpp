c00c1f1e7507143c16d6e8f3da0280a3d87f197c
wangying <17199883@qq.com>
2017-09-05 17:27:10 +0800

    update codes for x-dce

--- x-dce/src/strategy.cpp
+++ x-dce/src/strategy.cpp
@@ -205,7 +205,8 @@ void Strategy::FeedMd(MDBestAndDeep_MY* md, int *sig_cnt, signal_t* sigs)
 	//	clog_warning("[%s] strategy latency:%d us", module_name_, latency); 
 
 		sigs[i].st_id = this->GetId();
-		clog_debug("[%s] signal: strategy id:%d; sig_id:%d; exchange:%d; symbol:%s; open_volume:%d; buy_price:%f; close_volume:%d; sell_price:%f; sig_act:%d; sig_openclose:%d; orig_sig_id:%d",
+		// TODO: debug
+		clog_info("[%s] FeedMd(MDBestAndDeep signal: strategy id:%d; sig_id:%d; exchange:%d; symbol:%s; open_volume:%d; buy_price:%f; close_volume:%d; sell_price:%f; sig_act:%d; sig_openclose:%d; orig_sig_id:%d",
 					module_name_, sigs[i].st_id, sigs[i].sig_id,
 					sigs[i].exchange, sigs[i].symbol, sigs[i].open_volume, sigs[i].buy_price,
 					sigs[i].close_volume, sigs[i].sell_price, sigs[i].sig_act, sigs[i].sig_openclose, sigs[i].orig_sig_id); 
@@ -229,7 +230,8 @@ void Strategy::FeedMd(MDOrderStatistic_MY* md, int *sig_cnt, signal_t* sigs)
 		//clog_warning("[%s] strategy latency:%d us", module_name_, latency); 
 
 		sigs[i].st_id = this->GetId();
-		clog_debug("[%s] signal: strategy id:%d; sig_id:%d; exchange:%d; symbol:%s; open_volume:%d; buy_price:%f; close_volume:%d; sell_price:%f; sig_act:%d; sig_openclose:%d; orig_sig_id:%d",
+		// TODO: debug
+		clog_info("[%s] FeedMd(MDOrderStatistic signal: strategy id:%d; sig_id:%d; exchange:%d; symbol:%s; open_volume:%d; buy_price:%f; close_volume:%d; sell_price:%f; sig_act:%d; sig_openclose:%d; orig_sig_id:%d",
 					module_name_, sigs[i].st_id, sigs[i].sig_id,
 					sigs[i].exchange, sigs[i].symbol, sigs[i].open_volume, sigs[i].buy_price,
 					sigs[i].close_volume, sigs[i].sell_price, sigs[i].sig_act, sigs[i].sig_openclose, sigs[i].orig_sig_id); 
@@ -242,7 +244,8 @@ void Strategy::feed_sig_response(signal_resp_t* rpt, symbol_pos_t *pos, int *sig
 	this->pfn_feedsignalresponse_(rpt, pos, sig_cnt, sigs);
 	for (int i = 0; i < *sig_cnt; i++ ){
 		sigs[i].st_id = GetId();
-		clog_debug("[%s] signal: strategy id:%d;  sig_id:%d; exchange:%d; symbol:%s; open_volume:%d; buy_price:%f; close_volume:%d; sell_price:%f; sig_act:%d; sig_openclose:%d; orig_sig_id:%d",
+		// TODO: debug
+		clog_info("[%s] feed_sig_respons esignal: strategy id:%d;  sig_id:%d; exchange:%d; symbol:%s; open_volume:%d; buy_price:%f; close_volume:%d; sell_price:%f; sig_act:%d; sig_openclose:%d; orig_sig_id:%d",
 					module_name_, sigs[i].st_id, sigs[i].sig_id,
 					sigs[i].exchange, sigs[i].symbol, sigs[i].open_volume, sigs[i].buy_price,
 					sigs[i].close_volume, sigs[i].sell_price, sigs[i].sig_act, sigs[i].sig_openclose, sigs[i].orig_sig_id); 
@@ -462,7 +465,6 @@ void Strategy::UpdatePosition(const TunnRpt& rpt, unsigned short sig_openclose, 
 	} //end if (rpt.MatchedAmount > 0)
 
 	if (rpt.OrderStatus==X1_FTDC_SPD_CANCELED ||
-		rpt.OrderStatus==X1_FTDC_SPD_FILLED ||
 		rpt.OrderStatus==X1_FTDC_SPD_PARTIAL_CANCELED ||
 		rpt.OrderStatus==X1_FTDC_SPD_ERROR ||
 		rpt.OrderStatus==X1_FTDC_SPD_IN_CANCELED){
