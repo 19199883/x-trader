3cf9fb4ce7f60825cb226458d8fec737817eab80
wangying <17199883@qq.com>
2017-09-06 21:51:46 +0800

    update codes for c-dce

--- c-dce/src/self_buy_sell_check.cpp
+++ c-dce/src/self_buy_sell_check.cpp
@@ -4,11 +4,6 @@
 #include <map>
 #include <unordered_map>
 #include <mutex>
-#include <pthread.h>
-
-#include "const_define.h"
-#include "my_cmn_log.h"
-#include "my_cmn_util_funcs.h"
 
 using namespace std;
 
@@ -16,14 +11,14 @@ struct OrderInfo
 {
 	bool valid;
     std::string code;
-    char buy_sell_flag;
+    TX1FtdcBuySellTypeType side;
     double price;
 
-    OrderInfo(const char* code_, char buy_sell_flag_, double price_)
+    OrderInfo(const char* code_, TX1FtdcBuySellTypeType side_, double price_)
         : code(code_)
     {
 		valid = false;
-        buy_sell_flag = buy_sell_flag_;
+        side = side_;
         price = price_;
     }
 };
@@ -43,17 +38,13 @@ using namespace std;
 
 void SelfBuySellCheck::Init()
 {
-    //pthread_spin_init(&s_self_match_mutex, 0);
 }
 
 RESULT_TYPE SelfBuySellCheck::TryReqOrderInsert(
-    const char * account,       // account
     OrderRefDataType order_ref,	// 报单引用
     const char * code, 			// 证券代码
     double price,				// 价格
-    char price_flag,			// 价格类型
-    char buy_sell_flag,			// 买卖方向
-    char order_type,			// 报单类型
+    TX1FtdcBuySellTypeType side,			// 买卖方向
     OrderRefDataType * opposite_serial_no	// 【输出】导致自成交对手单的报单引用
     )
 {
@@ -66,10 +57,9 @@ RESULT_TYPE SelfBuySellCheck::TryReqOrderInsert(
 	MyOrderMap & my_orders = it->second;
 	for (MyOrderMapValue &value : my_orders){
 		// 相同合约，方向相反
-		if (value.second.code == code && buy_sell_flag != value.second.buy_sell_flag){
-			//// 现在高价买入，或低价卖出 (有非对价下单，需要判断更好价格，modified by chenyongshun 20131220)
-			if ((buy_sell_flag == CONST_ENTRUST_BUY && (price + DOUBLE_CHECH_PRECISION) >= value.second.price)
-				|| (buy_sell_flag != CONST_ENTRUST_BUY && (price - DOUBLE_CHECH_PRECISION) <= value.second.price)){
+		if (value.second.code == code && buy_sell_flag != value.second.side){
+			if ((side == X1_FTDC_SPD_BUY && (price + DOUBLE_CHECH_PRECISION) >= value.second.price) || 
+				(side != X1_FTDC_SPD_BUY && (price - DOUBLE_CHECH_PRECISION) <= value.second.price)){
 				ret = COMPLIANCE_CHECK_RESULT_SELFTRADE;
 				*opposite_serial_no = value.first;
 				TNL_LOG_WARN("OrderRef(%lld) of Acc(%s) maybe matched with OrderRef(%lld) of Acc(%s) ",
@@ -81,14 +71,14 @@ RESULT_TYPE SelfBuySellCheck::TryReqOrderInsert(
 
 	if ((ret == COMPLIANCE_CHECK_RESULT_SUCCESS)
 		&& (my_orders.find(order_ref) == my_orders.end())){
-		my_orders.insert(std::make_pair(order_ref, OrderInfo(code, buy_sell_flag, price)));
+		my_orders.insert(std::make_pair(order_ref, OrderInfo(code, side, price)));
 	}
 
     return ret;
 }
 
 
-inline void RemoveTerminateOrder(const char * account, OrderRefDataType order_ref)
+inline void RemoveTerminateOrder( OrderRefDataType order_ref)
 {
     AccOrderMap::iterator it = s_orders.find(account);
     if (it == s_orders.end())
@@ -99,26 +89,17 @@ inline void RemoveTerminateOrder(const char * account, OrderRefDataType order_re
     my_orders.erase(order_ref);
 }
 
-void SelfBuySellCheck::OnOrderInsertFailed(
-    const char * account,       // account
-    OrderRefDataType order_ref  // 报单引用
-    )
+void SelfBuySellCheck::OnOrderInsertFailed( OrderRefDataType order_ref)
 {
     return RemoveTerminateOrder(account, order_ref);
 }
 
-void SelfBuySellCheck::OnOrderFilled(
-    const char * account,       // account
-    OrderRefDataType order_ref  // 报单引用
-    )
+void SelfBuySellCheck::OnOrderFilled( OrderRefDataType order_ref)
 {
     return RemoveTerminateOrder(account, order_ref);
 }
 
-void SelfBuySellCheck::OnOrderCanceled(
-    const char * account,       // account
-    OrderRefDataType order_ref  // 报单引用
-    )
+void SelfBuySellCheck::OnOrderCanceled( OrderRefDataType order_ref)
 {
     return RemoveTerminateOrder(account, order_ref);
 }
