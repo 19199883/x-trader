e9ae97edcf67c6fa81d66e940f1bbc3f4a7db515
wangying <17199883@qq.com>
2017-09-07 11:32:32 +0800

    update codes for c-dce

--- c-dce/src/compliance.cpp
+++ c-dce/src/compliance.cpp
@@ -1,35 +1,45 @@
-﻿Compliance::Compliance():
-	min_counter_(0), max_counter_(0)
+﻿#include <string.h>
+#include <stdio.h>
+#include "compliance.h"
+
+Compliance::Compliance(): min_counter_(0), max_counter_(0)
 {
+	memset(ord_buffer_, 0, sizeof(ord_buffer_));
+	for(int i = 0; i < COUNTER_UPPER_LIMIT; i++){
+		ord_buffer_[i].valid = false;
+	}
 }
 
-int Compliance::TryReqOrderInsert(int ord_counter, const char * code,
-			double price,	TX1FtdcBuySellTypeType side)
+bool Compliance::TryReqOrderInsert(int ord_counter, const char * contract,
+			double price, TX1FtdcBuySellTypeType side)
 {
-    int ret = COMPLIANCE_CHECK_RESULT_SUCCESS;
+    bool ret = true;
 
-	AccOrderMap::iterator it = s_orders.find(account);
-	if (it == s_orders.end()){
-		it = s_orders.insert(make_pair(account, MyOrderMap())).first;
-	}
-	MyOrderMap & my_orders = it->second;
-	for (MyOrderMapValue &value : my_orders){
-		// 相同合约，方向相反
-		if (value.second.code == code && buy_sell_flag != value.second.side){
-			if ((side == X1_FTDC_SPD_BUY && (price + DOUBLE_CHECH_PRECISION) >= value.second.price) || 
-				(side != X1_FTDC_SPD_BUY && (price - DOUBLE_CHECH_PRECISION) <= value.second.price)){
-				ret = COMPLIANCE_CHECK_RESULT_SELFTRADE;
-				*opposite_serial_no = value.first;
+	for (int i = min_counter_; i<= max_counter_; i++){
+		OrderInfo& ord = ord_buffer_[i];
+		if (!ord.valid) continue;
+
+		if (strcmp(ord.contract, contract)==0 && side != ord.side){
+			if ((side == X1_FTDC_SPD_BUY && (price + DOUBLE_CHECH_PRECISION) >= ord.price) || 
+				(side != X1_FTDC_SPD_BUY && (price - DOUBLE_CHECH_PRECISION) <= ord.price)){
+				ret = false;
+
 				TNL_LOG_WARN("OrderRef(%lld) of Acc(%s) maybe matched with OrderRef(%lld) of Acc(%s) ",
 					order_ref, account, value.first, account);
 				break;
 			}
-		}
-	}
+		} // if (strcmp(ord.contract, contract)==0 && side != ord.side)
+	} // for (int i = min_counter_; i< max_counter_; i++)
 
-	if ((ret == COMPLIANCE_CHECK_RESULT_SUCCESS)
-		&& (my_orders.find(order_ref) == my_orders.end())){
-		my_orders.insert(std::make_pair(order_ref, OrderInfo(code, side, price)));
+	if (ret){
+		if (0 == min_counter_) min_counter_ = ord_counter;
+		max_counter_ = ord_counter;
+
+		OrderInfo& ord = ord_buffer_[ord_counter];
+		ord.valid = true;
+		strcmp(ord.contract, contract);
+		ord.side = side;
+		ord.price = price;
 	}
 
     return ret;
--- c-dce/src/compliance.h
+++ c-dce/src/compliance.h
@@ -7,39 +7,23 @@
 // 一个trader支持最多信号数 
 #define COUNTER_UPPER_LIMIT 20000
 
-
 struct OrderInfo
 {
 	bool valid;
-    std::string code;
+    char contract[10];
     TX1FtdcBuySellTypeType side;
     double price;
-
-    OrderInfo(const char* code_, TX1FtdcBuySellTypeType side_, double price_)
-        : code(code_)
-    {
-		valid = false;
-        side = side_;
-        price = price_;
-    }
 };
-
-// 定义别名，方便更换
-typedef unordered_map<OrderRefDataType, OrderInfo> MyOrderMap;
-typedef unordered_map<string, MyOrderMap> AccOrderMap;
-typedef AccOrderMap::value_type AccOrderMapValue;
-typedef MyOrderMap::value_type MyOrderMapValue;
-typedef MyOrderMap::iterator MyOrderMapIt;
-
-static AccOrderMap s_orders;
 
 class Compliance
 {
 	public:
 		Compliance();
 		//
-		// 下单尝试，增加报单委托/增加 开仓计数
-		int TryReqOrderInsert(int ord_counter, const char * code, double price,
+		/*
+		 * 返回值：true表示成功；false表示失败 
+		 */
+		bool TryReqOrderInsert(int ord_counter, const char * contract, double price,
 					TX1FtdcBuySellTypeType side);
 
 		// 下单失败回报，删除该报单/回滚开仓计数
@@ -59,7 +43,6 @@ class Compliance
 		// 存委托单信息，用于检查自成交。只有valid==true，才表示是有效的数据。
 		// 存储方法：LocalOrderID的counter部分作为数组下标进行存储
 		OrderInfo ord_buffer_[COUNTER_UPPER_LIMIT];
-		
 };
 
 #endif // SELFBUYSELLCHECK_H_
