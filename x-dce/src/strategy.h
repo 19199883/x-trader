47aea1a50cb1ceca5c28a154613a5ad609fbc50d
wangying <17199883@qq.com>
2017-10-18 18:05:07 +0800

    update codes for x-dce:修正初始仓位bug：在初始化时，没有对pos_cache_进行初始化，造成当回报时如果没有成交时当前仓位不会推送给策略

--- x-dce/src/strategy.h
+++ x-dce/src/strategy.h
@@ -185,7 +185,7 @@ private:
 	 */
 	void UpdateSigrptByTunnrpt(signal_resp_t& sigrpt, const TunnRpt& tunnrpt);
 	void UpdatePosition(const TunnRpt& rpt, unsigned short sig_openclose, unsigned short int sig_act);
-	void FillPositionRpt(const TunnRpt& rpt, position_t& pos);
+	void FillPositionRpt(position_t& pos);
 	const char * GetSymbol();
 	bool Freeze(unsigned short sig_openclose, unsigned short int sig_act, int32_t updated_vol);
 };
