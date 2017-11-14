4eb05cfc42b5bd524de153e1f70b7eea4518b4d6
wangying <17199883@qq.com>
2017-11-14 20:15:40 +0800

    x-zce:集成行情源代码到trader

--- x-zce/src/tap_md_producer.cpp
+++ x-zce/src/tap_md_producer.cpp
@@ -63,9 +63,18 @@ void TapMDProducer::InitApi()
 
 void TapMDProducer::Login()
 {
-	// TODO: to here
-	// TODO:
-		// TODO: refer to login of tunnel
+    TapAPITradeLoginAuth stLoginAuth;
+    memset(&stLoginAuth, 0, sizeof(stLoginAuth));
+    strcpy(stLoginAuth.UserNo, config_.userid.c_str());
+    strcpy(stLoginAuth.Password, config_.password.c_str());
+    stLoginAuth.ISModifyPassword = APIYNFLAG_NO;
+    stLoginAuth.ISDDA = APIYNFLAG_NO;
+    result = api_->Login(&stLoginAuth);
+    if (TAPIERROR_SUCCEED != result) {
+        clog_error("[%s] Login Error, result:%d",module_name_,result);
+    }else{
+        clog_warning("[%s] Login success", module_name_);
+	}
 }
 
 void TapMDProducer::ParseConfig()
@@ -75,18 +84,37 @@ void TapMDProducer::ParseConfig()
     TiXmlElement *RootElement = config.RootElement();    
 
 	// yield strategy
-    TiXmlElement *comp_node = RootElement->FirstChildElement("Disruptor");
-	if (comp_node != NULL){
-		strcpy(config_.yield, comp_node->Attribute("yield"));
+    TiXmlElement *disruptor_node = RootElement->FirstChildElement("Disruptor");
+	if (disruptor_node != NULL){
+		strcpy(config_.yield, disruptor_node->Attribute("yield"));
 	} else { clog_error("[%s] x-trader.config error: Disruptor node missing.", module_name_); }
+
+	// addr
+    TiXmlElement *l1md_node = RootElement->FirstChildElement("L1Md");
+	if (l1md_node != NULL){
+		config_.addr = l1md_node->Attribute("addr");
+	} else { clog_error("[%s] x-trader.config error: L1Md node missing.", module_name_); }
+	
+	// contracts file
+    TiXmlElement *contracts_file_node = RootElement->FirstChildElement("Subscription");
+	if (contracts_file_node != NULL){
+		strcpy(config_.contracts_file, contracts_file_node->Attribute("contracts"));
+	} else { clog_error("[%s] x-trader.config error: Subscription node missing.", module_name_); }
+
+	size_t ipstr_start = config_.addr.find("//")+2;
+	size_t ipstr_end = config_.addr.find(":",ipstr_start);
+	strcpy(config_.ip, config_.addr.substr(ipstr_start,ipstr_end-ipstr_start).c_str());
+	config_.port = stoi(config_.addr.substr(ipstr_end+1));
 }
 
 TapMDProducer::~TapMDProducer(){
-	if (md_provider_ != NULL){
-		delete md_provider_;
-		md_provider_ = NULL;
-		clog_warning("[%s] release md_provider.", module_name_);
-	}
+    if (api_){
+        /* 退出行情登录 */
+        MY_LOG_INFO("TAP - cancel subscribe...");
+        api_->Disconnect();
+    }
+
+    if (sID) { delete sID; }
 }
 
 MYQuoteData* TapMDProducer::build_quote_provider(SubscribeContracts &subscription) {
@@ -152,4 +180,184 @@ ZCEL2QuotSnapshotField_MY* TapMDProducer::GetL2QuoteSnapshot(int32_t index)
 	return &l2quotesnapshot_buffer_[index];
 }
 
+void TapMDProducer::OnRspLogin(TAPIINT32 errorCode, const TapAPIQuotLoginRspInfo *info)
+{
+    clog_warning("[%s] TAP - OnRspLogin", module_name_);
+    if (0 == errorCode){
+    } else {
+        clog_error("[%s] TAP - login failed, ErrorCode = %d", module_name_, errorCode);
+    }
+}
 
+// 交易所(ZCE)，品种类型硬编码(F)
+// 品种，日期从合约读取,e.g. SR
+// contract：801
+void TapMDProducer::OnAPIReady()
+{
+    MY_LOG_INFO("TAP - OnAPIReady");
+
+	std::string exchange_no = "ZCE";
+	char commodity_type = TAPI_COMMODITY_TYPE_FUTURES;
+
+	std::ifstream is;
+	is.open (config_.contracts_file);
+	string contrs = "";
+	if (is) {
+		getline(is, contrs);
+		contrs += " ";
+		size_t start_pos = 0;
+		size_t end_pos = 0;
+		string contr = "";
+		while ((end_pos=contrs.find(" ",start_pos)) != string::npos){
+			contr = contrs.substr (start_pos, end_pos-start_pos);
+
+			string commodity_no = contr.substr(0, 2);
+			string contract_no = contr.substr(2, 3);;
+
+			TapAPIContract tmp;
+			strcpy(tmp.Commodity.ExchangeNo, exchange_no.c_str());
+			tmp.Commodity.CommodityType = commodity_type;
+			strcpy(tmp.Commodity.CommodityNo, commodity_no.c_str());
+			strcpy(tmp.ContractNo1, contract_no.c_str());
+			tmp.CallOrPutFlag1 = TAPI_CALLPUT_FLAG_NONE;
+			tmp.CallOrPutFlag2 = TAPI_CALLPUT_FLAG_NONE;
+			int ret = api_->SubscribeQuote(sID, &tmp);
+			if(ret != 0){
+				clog_error("[%s] TAP - SubscribeQuote failed, ExchangeNo is %s, CommodityNo is %s,"
+					"ContractNo is %s, errorCode is %d.",
+					module_name_, tmp.Commodity.ExchangeNo, tmp.Commodity.CommodityNo, 
+					tmp.ContractNo1, ret);
+			}
+
+			clog_warning("TAP - SubscribeQuote successful, ExchangeNo is %s, CommodityNo is %s,"
+				"ContractNo is %s.", 
+				tmp.Commodity.ExchangeNo, tmp.Commodity.CommodityNo, tmp.ContractNo1);
+
+			start_pos = end_pos + 1;
+		}
+	}else { 
+		clog_error("[%s] can't open: %s", module_name_, config_.contracts_file); 
+	}
+}
+
+
+void TapMDProducer::OnDisconnect(TAPIINT32 reasonCode)
+{
+    clog_error("[%s] TAP - OnDisconnect, reasonCode is %d, reconnecting.", module_name_, reasonCode);
+}
+
+void TapMDProducer::OnRspQryExchange(TAPIUINT32 sessionID, TAPIINT32 errorCode, 
+	TAPIYNFLAG isLast, const TapAPIExchangeInfo *info) { }
+
+void TapMDProducer::OnRspQryCommodity(TAPIUINT32 sessionID, TAPIINT32 errorCode, TAPIYNFLAG isLast,
+	const TapAPIQuoteCommodityInfo *info) { }
+
+void TapMDProducer::OnRspQryContract(TAPIUINT32 sessionID, TAPIINT32 errorCode, TAPIYNFLAG isLast,
+	const TapAPIQuoteContractInfo *info) { }
+
+void TapMDProducer::OnRtnContract(const TapAPIQuoteContractInfo *info) { }
+
+void TapMDProducer::OnRspSubscribeQuote(TAPIUINT32 sessionID, TAPIINT32 errorCode, 
+	TAPIYNFLAG isLast, const TapAPIQuoteWhole *info)
+{
+    clog_info("[%s] TAP - OnRspSubscribeQuote", module_name_);
+
+    if (errorCode == 0 && NULL != info){
+		TapAPIQuoteWhole_MY data_my = Convert(*info);
+
+		// TODO: disruptor
+
+        clog_info("[%s] TAP - OnRspSubscribeQuote Successful, ExchangNo is %s, "
+			"CommodityNo is %s, ContractNo is %s.", module_name_,
+            info->Contract.Commodity.ExchangeNo, info->Contract.Commodity.CommodityNo, 
+			info->Contract.ContractNo1);
+    }else{
+        clog_error("[%s] TAP - SubscribeQuote failed, the error code is %d.", nodule_name_,
+			errorCode);
+    }
+}
+
+
+void TapMDProducer::OnRtnQuote(const TapAPIQuoteWhole *info)
+{
+    clog_info("[%s] TAP - OnRtnQuote", module_name_);
+    if ( NULL != info) {
+		TapAPIQuoteWhole_MY data_my = Convert(*info);
+
+		// TODO: disruptor
+		//
+        clog_info("[%s] TAP - OnRtnQuote Successful, ExchangNo is %s, CommodityNo is %s,"
+			"ContractNo is %s.",
+            info->Contract.Commodity.ExchangeNo, info->Contract.Commodity.CommodityNo, 
+			info->Contract.ContractNo1);
+    } else {
+        clog_error("[%s] TAP - RtnQuote failed, the pointer is null.", module_name_);
+    }
+}
+
+void TapMDProducer::Convert(const TapAPIQuoteWhole &other, TapAPIQuoteWhole_MY &data)
+{
+    strcpy(data.ExchangeNo, other.Contract.Commodity.ExchangeNo);
+    data.CommodityType = other.Contract.Commodity.CommodityType;
+    strcpy(data.CommodityNo, other.Contract.Commodity.CommodityNo);
+    strcpy(data.ContractNo1, other.Contract.ContractNo1);
+    strcpy(data.StrikePrice1, other.Contract.StrikePrice1);
+    data.CallOrPutFlag1 = other.Contract.CallOrPutFlag1;
+    strcpy(data.ContractNo2, other.Contract.ContractNo2);
+    strcpy(data.StrikePrice2, other.Contract.StrikePrice2);
+    data.CallOrPutFlag2 = other.Contract.CallOrPutFlag2;
+
+    strcpy(data.CurrencyNo, other.CurrencyNo);
+    data.TradingState = other.TradingState;
+    strcpy(data.DateTimeStamp, other.DateTimeStamp);
+
+    data.QPreClosingPrice = other.QPreClosingPrice;
+    data.QPreSettlePrice = other.QPreSettlePrice;
+    data.QPrePositionQty = other.QPrePositionQty;
+    data.QOpeningPrice = other.QOpeningPrice;
+    data.QLastPrice = other.QLastPrice;
+    data.QHighPrice = other.QHighPrice;
+    data.QLowPrice = other.QLowPrice;
+    data.QHisHighPrice = other.QHisHighPrice;
+    data.QHisLowPrice = other.QHisLowPrice;
+    data.QLimitUpPrice = other.QLimitUpPrice;
+    data.QLimitDownPrice = other.QLimitDownPrice;
+    data.QTotalQty = other.QTotalQty;
+    data.QTotalTurnover = other.QTotalTurnover;
+    data.QPositionQty = other.QPositionQty;
+    data.QAveragePrice = other.QAveragePrice;
+    data.QClosingPrice = other.QClosingPrice;
+    data.QSettlePrice = other.QSettlePrice;
+    data.QLastQty = other.QLastQty;
+    for(int i = 0; i < 20; i++) {
+        data.QBidPrice[i] = other.QBidPrice[i];
+        data.QBidQty[i] = other.QBidQty[i];
+        data.QAskPrice[i] = other.QAskPrice[i];
+        data.QAskQty[i] = other.QAskQty[i];
+    }
+    data.QImpliedBidPrice = other.QImpliedBidPrice;
+    data.QImpliedBidQty = other.QImpliedBidQty;
+    data.QImpliedAskPrice = other.QImpliedAskPrice;
+    data.QImpliedAskQty = other.QImpliedAskQty;
+    data.QPreDelta = other.QPreDelta;
+    data.QCurrDelta = other.QCurrDelta;
+    data.QInsideQty = other.QInsideQty;
+    data.QOutsideQty = other.QOutsideQty;
+    data.QTurnoverRate = other.QTurnoverRate;
+    data.Q5DAvgQty = other.Q5DAvgQty;
+    data.QPERatio = other.QPERatio;
+    data.QTotalValue = other.QTotalValue;
+    data.QNegotiableValue = other.QNegotiableValue;
+    data.QPositionTrend = other.QPositionTrend;
+    data.QChangeSpeed = other.QChangeSpeed;
+    data.QChangeRate = other.QChangeRate;
+    data.QChangeValue = other.QChangeValue;
+    data.QSwing = other.QSwing;
+    data.QTotalBidQty = other.QTotalBidQty;
+    data.QTotalAskQty = other.QTotalAskQty;
+}
+
+void TapMDProducer::OnRspUnSubscribeQuote(TAPIUINT32 sessionID, TAPIINT32 errorCode, 
+	TAPIYNFLAG isLast, const TapAPIContract *info) { }
+
+void TapMDProducer::OnRspChangePassword(TAPIUINT32 sessionID, TAPIINT32 errorCode) { } 
--- x-zce/src/tap_md_producer.h
+++ x-zce/src/tap_md_producer.h
@@ -38,7 +38,11 @@ class TapMDProducer : public ITapQuoteAPINotify
 		void End();
 
 	private:
-		MYQuoteData* build_quote_provider(SubscribeContracts &subscription);
+		ITapQuoteAPI *api_;
+		unsigned int *sID;
+
+		void InitApi();
+		void Login();
 		
 		void OnMD(const ZCEL2QuotSnapshotField_MY* md);
 		int32_t push(const ZCEL2QuotSnapshotField_MY& md);
@@ -52,6 +56,120 @@ class TapMDProducer : public ITapQuoteAPINotify
 
 		struct vrt_producer  *producer_;
 		std::array<ZCEL2QuotSnapshotField_MY, MD_BUFFER_SIZE> l2quotesnapshot_buffer_;
+
+		void Convert(const TapAPIQuoteWhole &other, TapAPIQuoteWhole_MY &my_data);
+
+		/*
+		 * API
+		 */
+		/**
+		 * @brief	系统登录过程回调。
+		 * @details	此函数为Login()登录函数的回调，调用Login()成功后建立了链路连接，然后API将向服务器发送登录认证信息，
+		 *			登录期间的数据发送情况和登录的回馈信息传递到此回调函数中。
+		 * @param[in] errorCode 返回错误码,0表示成功。
+		 * @param[in] info 登陆应答信息，如果errorCode!=0，则info=NULL。
+		 * @attention	该回调返回成功，说明用户登录成功。但是不代表API准备完毕。
+		 * @ingroup G_Q_Login
+		 */
+		virtual void TAP_CDECL OnRspLogin(TAPIINT32 errorCode, const TapAPIQuotLoginRspInfo *info);
+
+		/**
+		 * @brief	通知用户API准备就绪。
+		 * @details	只有用户回调收到此就绪通知时才能进行后续的各种行情数据查询操作。\n
+		 *			此回调函数是API能否正常工作的标志。
+		 * @attention  就绪后才可以进行后续正常操作
+		 * @ingroup G_Q_Login
+		 */
+		virtual void TAP_CDECL OnAPIReady();
+
+		/**
+		 * @brief	API和服务失去连接的回调
+		 * @details	在API使用过程中主动或者被动与服务器服务失去连接后都会触发此回调通知用户与服务器的连接已经断开。
+		 * @param[in] reasonCode 断开原因代码。 \n
+		 * @ingroup G_Q_Disconnect
+		 */
+		virtual void TAP_CDECL OnDisconnect(TAPIINT32 reasonCode);
+
+		/**
+		 * @brief 返回交易所信息
+		 * @param[in] sessionID 请求的会话ID；
+		 * @param[in] errorCode 错误码，当errorCode!=0时,info为NULL；
+		 * @param[in] isLast 标示是否是最后一批数据；
+		 * @param[in] info 返回的交易所信息。
+		 * @attention  不要修改和删除info所指示的数据；函数调用结束，参数不再有效。
+		 * @ingroup G_Q_Exchange
+		 */
+		virtual void TAP_CDECL OnRspQryExchange(TAPIUINT32 sessionID, TAPIINT32 errorCode, TAPIYNFLAG isLast, const TapAPIExchangeInfo *info);
+
+		/**
+		 * @brief	返回所有品种信息。
+		 * @details	此回调接口用于向用户返回得到的所有品种信息。
+		 * @param[in] sessionID 请求的会话ID，和GetAllCommodities()函数返回对应；
+		 * @param[in] errorCode 错误码，当errorCode!=0时,info为NULL；
+		 * @param[in] isLast 标示是否是最后一批数据；
+		 * @param[in] info 返回的信息数组的起始指针。
+		 * @attention  不要修改和删除info所指示的数据；函数调用结束，参数不再有效。
+		 * @ingroup G_Q_Commodity
+		 */
+		virtual void TAP_CDECL OnRspQryCommodity(TAPIUINT32 sessionID, TAPIINT32 errorCode, TAPIYNFLAG isLast, const TapAPIQuoteCommodityInfo *info);
+
+		/**
+		 * @brief 返回系统中合约信息
+		 * @param[in] sessionID 请求的会话ID；
+		 * @param[in] errorCode 错误码，当errorCode!=0时,info为NULL；
+		 * @param[in] isLast 标示是否是最后一批数据；
+		 * @param[in] info		指向返回的信息结构体。当errorCode不为0时，info为空。
+		 * @attention 不要修改和删除info所指示的数据；函数调用结束，参数不再有效。
+		 * @ingroup G_Q_Contract
+		 */
+		virtual void TAP_CDECL OnRspQryContract(TAPIUINT32 sessionID, TAPIINT32 errorCode, TAPIYNFLAG isLast, const TapAPIQuoteContractInfo *info);
+
+		/**
+		 * @brief	返回订阅行情的全文。
+		 * @details	此回调接口用来返回订阅行情的全文。全文为当前时间的行情信息。
+		 * @param[in] sessionID 请求的会话ID；
+		 * @param[in] isLast 标示是否是最后一批数据；
+		 * @param[in] errorCode 错误码，当errorCode!=0时,info为NULL；
+		 * @param[in] info		指向返回的信息结构体。当errorCode不为0时，info为空。
+		 * @ingroup G_Q_Quote
+		 */
+		virtual void TAP_CDECL OnRspSubscribeQuote(TAPIUINT32 sessionID, TAPIINT32 errorCode, TAPIYNFLAG isLast, const TapAPIQuoteWhole *info);
+
+		/**
+		 * @brief 退订指定合约的行情的结果回调
+		 * @param[in] sessionID 请求的会话ID；
+		 * @param[in] errorCode 错误码，当errorCode!=0时,info为NULL；
+		 * @param[in] isLast 标示是否是最后一批数据；
+		 * @param[in] info		指向返回的信息结构体。当errorCode不为0时，info为空。
+		 * @ingroup G_Q_Quote
+		 */
+		virtual void TAP_CDECL OnRspUnSubscribeQuote(TAPIUINT32 sessionID, TAPIINT32 errorCode, TAPIYNFLAG isLast, const TapAPIContract *info);
+
+		/**
+		 * @brief	返回订阅行情的变化内容。
+		 * @details	此回调接口用来通知用户行情信息产生了变化，并向用户提交新的行情全文。
+		 * @param[in] info 最新的行情全文内容
+		 * @attention 不要修改和删除Quote指示的数据；函数调用结束，参数不再有效。
+		 * @ingroup G_Q_Quote
+		 */
+		virtual void TAP_CDECL OnRtnQuote(const TapAPIQuoteWhole *info);
+
+		/**
+		 * @brief 通知用户密码修改结果
+		 * @param[in] sessionID 修改密码的会话ID,和ChangePassword返回的会话ID对应。
+		 * @param[in] errorCode 返回错误码，0表示成功。
+		 * @note 暂未实现
+		 * @ingroup G_Q_Password
+		 */
+		virtual void TAP_CDECL OnRspChangePassword(TAPIUINT32 sessionID, TAPIINT32 errorCode);
+
+		/**
+		 * @brief 通知合约添加
+		 * @param[in] info		指向返回的新增合约
+		 * @attention 不要修改和删除info所指示的数据；函数调用结束，参数不再有效。
+		 * @ingroup G_Q_Contract
+		 */
+		virtual void TAP_CDECL OnRtnContract(const TapAPIQuoteContractInfo *info);
 };
 
 #endif
