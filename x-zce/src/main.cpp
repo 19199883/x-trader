ed75b259bb2816dadc3ec1137695d877c0eaa2d0
wangying <17199883@qq.com>
2018-02-01 20:03:37 +0800

    x-zce micro

--- x-zce/src/main.cpp
+++ x-zce/src/main.cpp
@@ -31,6 +31,20 @@ int main(/*int argc, const char **argv*/)
 {
 #ifdef LATENCY_MEASURE
 	clog_warning("latency measure on"); 
+#else
+	clog_warning("latency measure off"); 
+#endif
+
+#ifdef COMPLIANCE_CHECK
+	clog_warning("COMPLIANCE_CHECK on"); 
+#else
+	clog_warning("COMPLIANCE_CHECK off"); 
+#endif
+
+#ifdef PERSISTENCE_ENABLED
+	clog_warning("PERSISTENCE_ENABLED on"); 
+#else
+	clog_warning("PERSISTENCE_ENABLEDon off"); 
 #endif
 
 	struct sigaction SIGINT_act;
