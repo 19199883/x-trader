9772e1411987f1207d46a31a850bf93b40cc6f7a
centos_vm <wangying@localhost.localdomain>
2018-03-29 18:53:52 +0800

    debug codes

--- x-shfe/src/l1md_producer.cpp
+++ x-shfe/src/l1md_producer.cpp
@@ -127,6 +127,9 @@ void L1MDProducer::OnRtnDepthMarketData(CDepthMarketDataField *data)
 
 	RalaceInvalidValue_Femas(*data);
 	
+	// TODO: debug
+	ToString(*data);
+
 	//clog_info("[%s] OnRtnDepthMarketData InstrumentID:%s,UpdateTime:%s,UpdateMillisec:%d",
 	//	module_name_,data->InstrumentID,data->UpdateTime,data->UpdateMillisec);
 
