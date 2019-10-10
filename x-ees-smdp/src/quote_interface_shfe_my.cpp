cacc859d5b89c3d2877a7c7d0f90b1617ae2f96b
wangying <17199883@qq.com>
2019-10-10 10:53:51 +0800

    ddd

--- x-ees-smdp/src/quote_interface_shfe_my.cpp
+++ x-ees-smdp/src/quote_interface_shfe_my.cpp
@@ -39,17 +39,57 @@ MYQuoteData::~MYQuoteData()
 
 void MYQuoteData::ProcEfhLev2Data(int32_t index)
 {
-	efh3_lev2* data = efhLev2Producer_->GetData(index);
+	efh3_lev2* efh_data = efhLev2Producer_->GetData(index);
 
-	CDepthMarketDataField* lev1_data = NULL;
+	CDepthMarketDataField* my_data = NULL;
 	if(l1_md_last_index_ != L1MD_NPOS){
-		 lev1_data =  l1_md_producer_->GetLastData(contract, l1_md_last_index_);
-		if(NULL != lev1_data){
-			// TODO: here
-
-			lev1_data-> = data->
-
-			RalaceInvalidValue(lev1_data);
+		 my_data =  l1_md_producer_->GetLastData(contract, l1_md_last_index_);
+		if(NULL != my_data){			
+			my_data->UpperLimitPrice = InvalidToZeroD(my_data->UpperLimitPrice);
+			my_data->LowerLimitPrice = InvalidToZeroD(my_data.LowerLimitPrice);
+			my_data->HighestPrice = InvalidToZeroD(my_data.HighestPrice);
+			my_data->LowestPrice = InvalidToZeroD(my_data.LowestPrice);
+			my_data->OpenPrice = InvalidToZeroD(my_data.OpenPrice);
+			my_data->ClosePrice = InvalidToZeroD(my_data.ClosePrice);
+			my_data->PreClosePrice = InvalidToZeroD(my_data.PreClosePrice);			
+			my_data->PreOpenInterest = InvalidToZeroD(my_data.PreOpenInterest);
+			my_data->SettlementPrice = InvalidToZeroD(my_data.SettlementPrice);
+			my_data->PreSettlementPrice = InvalidToZeroD(my_data.PreSettlementPrice);			
+			my_data->PreDelta = InvalidToZeroD(my_data.PreDelta);
+			my_data->CurrDelta = InvalidToZeroDmy_datad.CurrDelta);
+			
+			my_data->LastPrice = InvalidToZeroD(efh_data->m_last_px);															
+			my_data->Volume = efh_data->m_last_share;
+			my_data->Turnover = InvalidToZeroD(efh_data->m_turnover);				
+			strcpy(my_data->UpdateTime,efh_data->m_update_time);
+			my_data->UpdateMillisec = efh_data->m_millisecond;
+			my_data->OpenInterest = InvalidToZeroD(efh_data->m_open_interest);	
+			
+			// my_data-> = efh_data->m_symbol_code;
+			
+			my_data->BidPrice1 =  InvalidToZeroD(efh_data->m_bid_1_px);
+			my_data->BidPrice2 =  InvalidToZeroD(efh_data->m_bid_2_px);
+			my_data->BidPrice3 =  InvalidToZeroD(efh_data->m_bid_3_px);
+			my_data->BidPrice4 =  InvalidToZeroD(efh_data->m_bid_4_px);
+			my_data->BidPrice5 =  InvalidToZeroD(efh_data->m_bid_5_px);
+			
+			my_data->BidVolume1 = efh_data->m_bid_1_share;
+			my_data->BidVolume2 = efh_data->m_bid_2_share;
+			my_data->BidVolume3 = efh_data->m_bid_3_share;
+			my_data->BidVolume4 = efh_data->m_bid_4_share;
+			my_data->BidVolume5 = efh_data->m_bid_5_share;
+				
+			my_data->AskPrice1 =  InvalidToZeroD(efh_data->m_ask_1_px);
+			my_data->AskPrice2 =  InvalidToZeroD(efh_data->m_ask_2_px);
+			my_data->AskPrice3 =  InvalidToZeroD(efh_data->m_ask_3_px);
+			my_data->AskPrice4 =  InvalidToZeroD(efh_data->m_ask_4_px);
+			my_data->AskPrice5 =  InvalidToZeroD(efh_data->m_ask_5_px);
+			
+			my_data->AskVolume1 = efh_data->m_ask_1_share;		
+			my_data->AskVolume2 = efh_data->m_ask_2_share;
+			my_data->AskVolume3 = efh_data->m_ask_3_share;		
+			my_data->AskVolume4 = efh_data->m_ask_4_share;
+			my_data->AskVolume5 = efh_data->m_ask_5_share;			
 
 			if (fulldepthmd_handler_ != NULL) { fulldepthmd_handler_(&target_data_); }
 
