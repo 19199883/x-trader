7d42c6aebfa4fb6f0256b0e1ad02cd306b811030
wangying <17199883@qq.com>
2019-11-12 18:38:03 +0800

    ff

--- shfe-ees-lev2/src/main.cpp
+++ shfe-ees-lev2/src/main.cpp
@@ -65,7 +65,7 @@ int main(/*int argc, const char **argv*/)
 	sigaction(SIGUSR2, &SIGINT_act, NULL);
 
 	// version
-	clog_warning("version: shfe-lev2-md_2019-11-09"); 
+	clog_warning("version: shfe-lev2-md_2019-11-12"); 
 	clog_warning("max contract count:%d",MAX_CONTRACT_COUNT ); 
 
 	struct vrt_queue  *queue;
