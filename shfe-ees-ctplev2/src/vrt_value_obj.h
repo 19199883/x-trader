53cf3aabf6b3d56ae1a39d9bba4c644b2d928325
wangying <17199883@qq.com>
2020-03-03 13:54:46 +0800

    rrr

--- shfe-ees-ctplev2/src/vrt_value_obj.h
+++ shfe-ees-ctplev2/src/vrt_value_obj.h
@@ -17,13 +17,10 @@
  * 如下宏根据情况进行定义
 */
 // 行情持久化开关
-#define PERSISTENCE_ENABLED
+//#define PERSISTENCE_ENABLED
 //#define LATENCY_MEASURE
 
-// 飞马极速行情
-#define FEMAS_TOPSPEED_QUOTE
 // 盛立极速行情 API multicast
-//#define EES_UDP_TOPSPEED_QUOTE
 
 // software license
 #define SERVER_NAME  "zjtest1"
